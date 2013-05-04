054a2d250637f796011c187ecd1aeba1c4f647b0
Paul Miller <paul@paulmillr.com>
2013-05-05 00:02:39 +0300

    Update to Chaplin 0.9.0.

--- js/controllers/base/controller.js
+++ js/controllers/base/controller.js
@@ -3,10 +3,8 @@ define(['chaplin', 'views/site-view'], function(Chaplin, SiteView) {
 
   var Controller = Chaplin.Controller.extend({
     // Place your application-specific controller features here.
-    beforeAction: {
-      '.*': function() {
-        this.compose('site', SiteView);
-      }
+    beforeAction: function() {
+      this.compose('site', SiteView);
     }
   });
 
