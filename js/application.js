9c5b879dd5dc2edfd839e3048dca631e56dd00f2
Paul Miller <paul@paulmillr.com>
2013-10-30 22:48:45 +0200

    Application#start comments via https://github.com/chaplinjs/chaplin/issues/629.

--- js/application.js
+++ js/application.js
@@ -6,7 +6,12 @@ define(['chaplin'], function(Chaplin) {
   var Application = Chaplin.Application.extend({
     // Set your application name here so the document title is set to
     // “Controller title – Site title” (see Layout#adjustTitle)
-    title: 'Chaplin Example Application'
+    title: 'Chaplin Example Application',
+    start: function() {
+      // You can fetch some data here and start app
+      // (by calling supermethod) after that.
+      Chaplin.Application.prototype.start.apply(this, arguments);
+    }
   });
 
   return Application;
