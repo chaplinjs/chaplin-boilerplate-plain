9209fd47ce407326642dbc7d4e7a008a3794fc54
mtak3 <mtak@tyrellsys.com>
2014-01-24 18:26:05 +0900

    subfolder対応
    https://github.com/chaplinjs/chaplin-boilerplate-plain#running-the-example

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,4 +1,4 @@
 // Bootstrap the application
 require(['application', 'routes'], function(Application, routes) {
-  new Application({routes: routes, controllerSuffix: '-controller'});
+  new Application({routes: routes, controllerSuffix: '-controller', root: location.pathname});
 });
